eddaf6ca36f47928c74283336af8c6cbe44eaeb9
dklaputa <d.k.laputa@gmail.com>
2017-05-23 12:08:10 +0900

    Increase add and remove buttons' range to make it easy to click. Fix a bug.

--- app/src/main/java/org/mushare/httper/HttpSettingListItem.java
+++ app/src/main/java/org/mushare/httper/HttpSettingListItem.java
@@ -109,13 +109,13 @@ public class HttpSettingListItem extends AbstractSectionableItem<HttpSettingList
         holder.imageButtonRemove.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                int position = holder.getAdapterPosition();
+                if (position < 0 || position >= adapter.getItemCount()) return;
                 if (adapter.getSectionItems(getHeader()).size() == 1) {
                     holder.textViewKey.setText(null);
                     holder.textViewValue.setText(null);
                     return;
                 }
-                int position = holder.getAdapterPosition();
-                if (position < 0 || position >= adapter.getItemCount()) return;
                 holder.textViewKey.clearFocus();
                 holder.textViewValue.clearFocus();
                 adapter.removeItem(position);
