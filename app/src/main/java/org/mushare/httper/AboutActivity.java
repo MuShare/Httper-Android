5ec337006a95bb93f273d47cd5bb94d79ad93abd
dklaputa <d.k.laputa@gmail.com>
2017-06-07 23:36:41 +0900

    About us activity.

---
+++ app/src/main/java/org/mushare/httper/AboutActivity.java
@@ -0,0 +1,72 @@
+package org.mushare.httper;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+/**
+ * Created by dklap on 6/7/2017.
+ */
+
+public class AboutActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        findViewById(R.id.imageButtonLink).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://httper" +
+                        ".mushare.cn/"));
+                startActivity(browserIntent);
+            }
+        });
+        findViewById(R.id.imageButtonGithub).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github" +
+                        ".com/MuShare/Httper-Android"));
+                startActivity(browserIntent);
+            }
+        });
+        findViewById(R.id.imageButtonStore).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String appPackageName = getPackageName();
+                try {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id="
+                            + appPackageName)));
+                } catch (android.content.ActivityNotFoundException e) {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google"
+                            + ".com/store/apps/details?id=" + appPackageName)));
+                }
+            }
+        });
+
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.linearLayoutAbout);
+        final String[] licenses = getResources().getStringArray(R.array.licenses);
+        if (licenses.length % 3 == 0) {
+            int i = 0;
+            while (i < licenses.length) {
+                LinearLayout item = (LinearLayout) LinearLayout.inflate(this, R.layout
+                        .list_license_item, null);
+                ((TextView) item.findViewById(R.id.license_name)).setText(licenses[i++]);
+                ((TextView) item.findViewById(R.id.license_detail)).setText(licenses[i++]);
+                final String url = licenses[i++];
+                item.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                        startActivity(browserIntent);
+                    }
+                });
+                linearLayout.addView(item);
+            }
+        }
+    }
+}
--- app/src/main/java/org/mushare/httper/MainFragment.java
+++ app/src/main/java/org/mushare/httper/MainFragment.java
@@ -90,38 +90,7 @@ public class MainFragment extends Fragment {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if (s.length() > 0 && s.toString().matches("^" +
-                        // user:pass authentication
-                        "(?:\\S+(?::\\S*)?@)?" +
-                        "(?:" +
-                        // IP address exclusion
-                        // private & local networks
-                        "(?!(?:10|127)(?:\\.\\d{1,3}){3})" +
-                        "(?!(?:169\\.254|192\\.168)(?:\\.\\d{1,3}){2})" +
-                        "(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})" +
-                        // IP address dotted notation octets
-                        // excludes loopback network 0.0.0.0
-                        // excludes reserved space >= 224.0.0.0
-                        // excludes network & broacast addresses
-                        // (first & last IP address of each class)
-                        "(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])" +
-                        "(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}" +
-                        "(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))" +
-                        "|" +
-                        // host name
-                        "(?:(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)" +
-                        // domain name
-                        "(?:\\.(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)*" +
-                        // TLD identifier
-                        "(?:\\.(?:[a-z\\u00a1-\\uffff]{2,}))" +
-                        // TLD may end with dot
-                        "\\.?" +
-                        ")" +
-                        // port number
-                        "(?::\\d{2,5})?" +
-                        // resource path
-                        "(?:[/?#]\\S*)?" +
-                        "$")) buttonSend.setEnabled(true);
+                if (s.length() > 0) buttonSend.setEnabled(true);
                 else buttonSend.setEnabled(false);
             }
 
@@ -253,6 +222,9 @@ public class MainFragment extends Fragment {
                     DialogFragment newFragment = new ClearRequestDialog();
                     newFragment.setTargetFragment(MainFragment.this, 0);
                     newFragment.show(getFragmentManager(), "dialog");
+                    return true;
+                } else if (item.getItemId() == R.id.menuAbout) {
+                    startActivity(new Intent(getContext(), AboutActivity.class));
                     return true;
                 }
                 return false;
